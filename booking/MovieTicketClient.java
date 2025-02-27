2f3576d13b7d95c1c835737bda3a724036c918a5
Daniel Alnes <daniealn@online.ntnu.no>
2025-02-27 12:06:57 +0100

    added simple classes without thread function yet.

--- booking/MovieTicketClient.java
+++ booking/MovieTicketClient.java
@@ -1,4 +1,18 @@
 package booking;
 
 public class MovieTicketClient {
+
+    private MovieTicketServer movieTicketServer;
+    private String name;
+    private int tickets;
+
+    public MovieTicketClient(MovieTicketServer movieTicketServer, String name, int tickets) {
+        this.movieTicketServer = movieTicketServer;
+        this.name = name;
+        this.tickets = tickets;
+    }
+
+    public void start() {
+        movieTicketServer.bookTickets(name, tickets);
+    }
 }
--- booking/MovieTicketServer.java
+++ booking/MovieTicketServer.java
@@ -2,5 +2,20 @@ package booking;
 
 
 public class MovieTicketServer {
+    private String movieName;
+    private int totalTickets;
 
+    public MovieTicketServer(String movieName, int totalTickets) {
+        this.movieName = movieName;
+        this.totalTickets = totalTickets;
+    }
+
+    public void bookTickets(String name, int tickets) {
+        if (tickets > totalTickets) {
+            System.out.println("Not enough tickets");
+        } else {
+            totalTickets -= tickets;
+            System.out.println("Booked");
+        }
+    }
 }
